ca5c62fb32c92ab6cf121cda87131518feae41c3
Jannis meierhoefer <jannism1@web.de>
2019-12-28 19:53:18 +0100

    -Added Prices Page Added Navigation Fragment

--- src/main/java/de/oth/Packlon/controller/HomeController.java
+++ src/main/java/de/oth/Packlon/controller/HomeController.java
@@ -16,63 +16,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 
 
-    @Autowired
-    private AccountService accountService;
-    @Autowired
-    private CustomerService customerService;
-
     @RequestMapping("/")
-    public String starten(Model model) {
-        Account account = new Account();
-        model.addAttribute("account", account);
+    public String starten() {
+
         return "index";
     }
-
-    @RequestMapping(value = "/register", method =  RequestMethod.POST)
-    public String register(
-            Account account,
-            @ModelAttribute("firstName") String firstName,
-            @ModelAttribute("lastName") String lastName,
-            Model model
-    ) {
-        boolean valid = true;
-
-        if (accountService.existsAccountWithEmail(account.getEmail())) {
-            valid = false;
-        }
-        if (firstName.length() < 3 || firstName.trim().isEmpty()) {
-            model.addAttribute(" firstNameErrorMessage", "Firstname has to be longer than 3 letters");
-            valid = false;
-        }
-
-        if (lastName.length() < 3 || lastName.trim().isEmpty()) {
-            valid = false;
-            model.addAttribute("lastNameErrorMessage", "Lastname has to be longer than 3 letters");
-        }
-        if (!valid)
-            return "index";
-
-
-        Customer customer = new Customer();
-        customer.setLastName(lastName);
-        customer.setFirstName(firstName);
-        customer = customerService.getCustomerByName(customer);
-        account.setOwner(customer);
-
-
-        if (valid) {
-            Account nAcc = new Account();
-            model.addAttribute("account", nAcc);
-            accountService.createAccount(account);
-        }
-
-        return "login";
-
-    }
-
-    @RequestMapping(value = {"/login"}, method = RequestMethod.GET)
-    public String login() {
-        return "login";
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
     }
 
 
